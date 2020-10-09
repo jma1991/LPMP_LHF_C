730ae1965d5e0db2e263983deaae81fc29a39d50
James Ashmore <jma1991@icloud.com>
2020-10-09 11:30:07 +0100

    publish

---
+++ _workflowr.sh
@@ -0,0 +1,9 @@
+git add _workflowr.yml _workflowr.R _workflowr.sh
+
+git add analysis/_site.yml analysis/about.Rmd analysis/index.Rmd analysis/license.Rmd analysis/01-quality-control.Rmd analysis/02-normalization.Rmd analysis/03-feature-selection.Rmd analysis/04-reduced-dimensions.Rmd analysis/05-clustering.Rmd analysis/06-doublet-detection.Rmd analysis/07-cell-cycle.Rmd analysis/08-marker-detection.Rmd analysis/09-cell-annotation.Rmd analysis/10-atlas-integration.Rmd analysis/11-atlas-neighbors.Rmd analysis/12-atlas-trajectory.Rmd analysis/13-atlas-clustering.Rmd analysis/14-atlas-markers.Rmd analysis/15-scenic-analysis.Rmd analysis/16-geneset-activity.Rmd analysis/17-haystack-analysis.Rmd
+
+git commit -m "publish"
+
+git add docs/about.html docs/index.html docs/license.html docs/01-quality-control.html docs/02-normalization.html docs/03-feature-selection.html docs/04-reduced-dimensions.html docs/05-clustering.html docs/06-doublet-detection.html docs/07-cell-cycle.html docs/08-marker-detection.html docs/09-cell-annotation.html docs/10-atlas-integration.html docs/11-atlas-neighbors.html docs/12-atlas-trajectory.html docs/13-atlas-clustering.html docs/14-atlas-markers.html docs/15-scenic-analysis.html docs/16-geneset-activity.html docs/17-haystack-analysis.html docs/figure/01-quality-control.Rmd docs/figure/02-normalization.Rmd docs/figure/03-feature-selection.Rmd docs/figure/04-reduced-dimensions.Rmd docs/figure/05-clustering.Rmd docs/figure/06-doublet-detection.Rmd docs/figure/07-cell-cycle.Rmd docs/figure/08-marker-detection.Rmd docs/figure/09-cell-annotation.Rmd docs/figure/10-atlas-integration.Rmd docs/figure/11-atlas-neighbors.Rmd docs/figure/12-atlas-trajectory.Rmd docs/figure/13-atlas-clustering.Rmd docs/figure/14-atlas-markers.Rmd docs/figure/15-scenic-analysis.Rmd docs/figure/16-geneset-activity.Rmd docs/figure/17-haystack-analysis.Rmd docs/site_libs docs/.nojekyll
+
+git push origin master
